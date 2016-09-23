8da5f8fd22d5c0c5f3942d01436c450f83029994
Matt Nicholas <fostermatt82@gmial.com>
2016-09-22 22:48:22 -0700

    add method with test to return stylist by id

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -11,4 +11,42 @@ public class Stylist {
     this.name = name;
   }
 
+  public String getName(){
+    return name;
+  }
+
+  public int getId(){
+    return id;
+  }
+
+  @Override
+  public boolean equals(Object otherStylist) {
+    if (!(otherStylist instanceof Stylist)) {
+      return false;
+    } else {
+      Stylist newStylist = (Stylist) otherStylist;
+      return this.getName().equals(newStylist.getName()) &&
+             this.getId() == newStylist.getId();
+    }
+  }
+
+  public static Stylist find(int id){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT * FROM stylists WHERE id=:id";
+      Stylist stylist = con.createQuery(sql)
+      .addParameter("id", id)
+      .executeAndFetchFirst(Stylist.class);
+      return stylist;
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO stylists (name) VALUES (:name)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .executeUpdate()
+        .getKey();
+    }
+  }
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -14,4 +14,28 @@ public class StylistTest {
     assertEquals(true, testStylist instanceof Stylist);
   }
 
+  @Test
+  public void getName_stylistInstantiatesWithName_Alana() {
+    Stylist testStylist = new Stylist("Alana");
+    assertEquals("Alana", testStylist.getName());
+  }
+
+  @Test
+   public void getId_stylistInstantiateWithAnId_1() {
+     Stylist testStylist = new Stylist("Alana");
+     testStylist.save();
+     assertTrue(testStylist.getId() > 0);
+   }
+   @Test
+   public void find_returnsAllStylits_stylist2 (){
+     Stylist stylist1 = new Stylist("Alana");
+     stylist1.save();
+     Stylist stylist2 = new Stylist("Sarah");
+     stylist2.save();
+     assertEquals(Stylist.find(stylist2.getId()), stylist2);
+   }
+
+
+
+
 }
